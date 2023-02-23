47e1d27f2ccfbc81929682d1c57da662c4ec955b
Shubham Gadekar <103500932+ShubhamGadekar027@users.noreply.github.com>
2023-02-23 12:26:09 +0530

    Add files via upload

--- Ecommerce_Case_Study.sql
+++ Ecommerce_Case_Study.sql
@@ -1,4 +1,4 @@
-Create Database Ecommerce_Case_Study;
+--Create Database Ecommerce_Case_Study;
 
 Use Ecommerce_Case_Study;
 
@@ -76,7 +76,6 @@ Group by Customer_ID;
 
 
 
-
 --What are my top 3 countries w.r.t Sales ?
 
 Select top 3 Country, sum(Sales)
@@ -84,19 +83,57 @@ From Sales
 Group by Country;
 
 
+--What is the average shipping cost for each state in the given year?
+
+Select  
+    state, 
+    AVG(shipping_cost) AS average_shipping_cost
+From Sales
+Where DATEPART(Year, order_date) = 2022
+Group by state
+Order by 1;
 
 
+-- Which customer segments contributed the most to the sales in the given year?
+
+SELECT 
+    segment, 
+    SUM(sales) AS total_sales
+From Sales
+Where DATEPART(Year, order_date) = 2022
+Group by segment
+Order by total_sales DESC;
 
 
+-- What is the average order processing time for each product category in the given year?
+
+SELECT 
+    product_category,
+    AVG(DATEDIFF(day, order_date, Shipping_Date)) AS average_processing_time
+From Sales
+Where DATEPART(YEAR, order_date) = 2022
+Group by product_category;
 
 
+-- How do my sales and profits compare between weekdays and weekends?
+
+SELECT 
+    Case 
+        When DATEPART(Weekday, order_date) IN (1, 7) Then 'Weekend'
+        Else 'Weekday'
+    End as day_type, 
+    Format(SUM(sales)/1000000, 'N') + 'Millions' AS total_sales, 
+    Format(SUM(profit)/1000000, 'N') + 'Millions' AS total_profit
+From Sales
+Where DATEPART(YEAR, order_date) = 2022
+Group by 
+    Case 
+        When DATEPART(Weekday, order_date) IN (1, 7) Then 'Weekend'
+        Else 'Weekday'
+    End
 
 
-
-
-
-
-
+-- 
 
 
 
